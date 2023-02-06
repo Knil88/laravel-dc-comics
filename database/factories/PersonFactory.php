2689607ce782f3d29d2f0533d28a7d8553832856
Yari <narducciyarigmail.com>
2023-02-06 16:17:50 +0100

    creazione dati faker

---
+++ database/factories/PersonFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Person>
+ */
+class PersonFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+          'name'=>fake()->firstName(),
+          'lastname'=>fake()->lastName(),
+          'date_of_birth'=>fake()->date(),
+          'height'=>fake()->numberBetween(0.00,2.20)
+        ];
+    }
+}
